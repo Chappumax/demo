2c327f7a6650ea2af7255c3a06655e9d65c1dba1
sapthami-devappsys <sapthami@devappsys.com>
2023-10-25 22:13:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,16 +2,14 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello, this is a GET request!');
+app.get('/api/getData', (req, res) => {
+    res.json({ message: 'GET request successful' });
 });
 
-app.post('/', (req, res) => {
-  res.send('Hello, this is a POST request!');
+app.post('/api/postData', (req, res) => {
+    res.json({ message: 'POST request successful' });
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+    console.log(`Server is running at http://localhost:${port}`);
 });
-
-
